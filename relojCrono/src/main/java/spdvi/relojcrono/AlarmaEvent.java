a3d397421674f0d46812c3bfb047278b8810154e
Berto <albertoredondo@paucasesnovescifp.cat>
2024-11-20 08:56:02 +0100

    reloj
    No sé que está pasando

---
+++ relojCrono/src/main/java/spdvi/relojcrono/AlarmaEvent.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spdvi.relojcrono;
+
+import java.util.EventObject;
+
+/**
+ *
+ * @author alber
+ */
+public class AlarmaEvent extends EventObject {
+
+    public AlarmaEvent(Object source) {
+        super(source);
+    }
+}
---
+++ relojCrono/src/main/java/spdvi/relojcrono/RelojCrono.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package spdvi.relojcrono;
+
+/**
+ *
+ * @author alber
+ */
+public class RelojCrono {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
